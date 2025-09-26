87bb2e230fb1b57144172757eeb756be303e14a0
enayahk <enayahk@nycstudents.net>
2025-09-26 08:41:33 -0400

    more code(probably works

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -6,16 +7,25 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner (System.in);
         System.out.println("Enter your total bill in dollars: $");
-        double billStart = (scan.nextDouble()*100);
+        double billStart = (scan.nextDouble()*100.0);
         System.out.println("How much of a tip do you want to pay?(percent):");
         double tipPercent = (scan.nextDouble()/100);
         System.out.println("How many people are paying?");
         int people = scan.nextInt();
+        System.out.println("---------------");
+        System.out.println(" ");
         double tipAmount = ((tipPercent * (billStart/100)));
-        System.out.println("Tip amount: $"+(tipAmount));
-        System.out.println("Total bill: $"+(((billStart)/100) + tipAmount));
-        System.out.println("Tip paid per person: $" +(tipAmount/people));
-        System.out.println("Total bill paid per person: $" +(((tipAmount + billStart)/100)/people));
+        System.out.println(" ");
+        System.out.printf("Tip amount: $"+ "%.2f",(tipAmount));
+        System.out.println(" ");
+        System.out.printf("Total bill: $"+ "%.2f", (((billStart)/100) + tipAmount));
+        System.out.println(" ");
+        System.out.printf("Tip paid per person: $" +"%.2f", +(tipAmount / people));
+        System.out.println(" ");
+        System.out.printf("Total bill paid per person: $" + "%.2f", +(((tipAmount + (billStart/100)))/people));
+        System.out.println(" ");
+        System.out.println("---------------");
+
 
 
 
